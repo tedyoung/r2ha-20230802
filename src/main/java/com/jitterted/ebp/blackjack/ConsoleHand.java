250adda63657e587a29a4db4963c306a2373c6e7
Ted M. Young <tedyoung@gmail.com>
2023-08-02 11:58:02 -0700

    LAB #2: Moved displayFaceUpCard() from Hand to ConsoleHand

---
+++ src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
@@ -0,0 +1,8 @@
+package com.jitterted.ebp.blackjack;
+
+public class ConsoleHand {
+
+    static String displayFaceUpCard(Hand hand) {
+        return ConsoleCard.display(hand.dealerFaceUpCard());
+    }
+}
--- src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -8,11 +8,11 @@ import static org.assertj.core.api.Assertions.*;
 
 public class HandDisplayTest {
 
-  @Test
-  public void displayFaceUpCard() throws Exception {
-    Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE)));
+    @Test
+    public void displayFaceUpCard() throws Exception {
+        Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE)));
 
-    assertThat(hand.displayFaceUpCard())
-        .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
-  }
+        assertThat(ConsoleHand.displayFaceUpCard(hand))
+                .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
+    }
 }

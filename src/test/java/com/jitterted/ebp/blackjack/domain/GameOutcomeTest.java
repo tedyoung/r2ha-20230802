d59bd5feb544d4708fe1f721d48bd1e67e4c4fad
Ted M. Young <tedyoung@gmail.com>
2023-08-02 16:44:23 -0700

    LAB #4: Added test for player beats dealer, extracting method to Encapsulate Setup.

--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -8,13 +8,29 @@ class GameOutcomeTest {
 
     @Test
     void playerHitsAndGoesBustThenOutcomeIsPlayerLoses() {
-        Game game = new Game(StubDeck.playerHitsAndGoesBust());
-        game.initialDeal();
+        Game game = createGameAndDoInitialDeal(StubDeck.playerHitsAndGoesBust());
 
         game.playerHits();
 
         assertThat(game.determineOutcome())
                 .isEqualTo("You Busted, so you lose.  💸");
+    }
+
+    @Test
+    void playerDealtBetterHandThanDealerAndStandsThenPlayerBeatsDealer() {
+        Game game = createGameAndDoInitialDeal(StubDeck.playerStandsAndBeatsDealer());
+
+        game.playerStands();
+        game.dealerTurn();
+
+        assertThat(game.determineOutcome())
+                .isEqualTo("You beat the Dealer! 💵");
+    }
+
+    private static Game createGameAndDoInitialDeal(Deck deck) {
+        Game game = new Game(deck);
+        game.initialDeal();
+        return game;
     }
 
 }

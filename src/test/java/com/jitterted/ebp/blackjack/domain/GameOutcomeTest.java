b3c53e0cfa4d76eb14acfadd73f62fcf90ee60b1
Ted M. Young <tedyoung@gmail.com>
2023-08-02 15:48:32 -0700

    First attempt at testing Game now that it's pure domain, but test is flaky.

---
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -0,0 +1,19 @@
+package com.jitterted.ebp.blackjack.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GameOutcomeTest {
+
+    @Test
+    void playerHitsAndGoesBustThenOutcomeIsPlayerLoses() {
+        Game game = new Game();
+        game.initialDeal();
+
+        game.playerHits();
+
+        assertThat(game.determineOutcome())
+                .isEqualTo("You Busted, so you lose.  💸");
+    }
+}

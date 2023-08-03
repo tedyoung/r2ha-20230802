02e7baa1d1280328ab18112b4e8bf598f64c6d45
Ted M. Young <tedyoung@gmail.com>
2023-08-03 15:13:23 -0700

    LAB #8B: Implement Player Stands scenario

--- src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
@@ -80,6 +80,7 @@ public class Game {
     public void playerStands() {
         playerDone = true;
         dealerTurn();
+        gameMonitor.roundCompleted(this);
     }
 
     public boolean isPlayerDone() {

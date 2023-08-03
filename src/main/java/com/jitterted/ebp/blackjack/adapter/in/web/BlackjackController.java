aceef0092ecc00922b8b1d3f8c361afedef9d7eb
Ted M. Young <tedyoung@gmail.com>
2023-08-03 11:29:10 -0700

    TDD: GET to /done endpoint returns 200 (template exists)

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -35,4 +35,10 @@ public class BlackjackController {
         }
         return "redirect:/game";
     }
+
+    @GetMapping("/done")
+    public String doneView() {
+        return "done";
+    }
+
 }
